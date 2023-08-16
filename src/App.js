1ed64b9a143bc21f0b79769579f7e76ddb12349f
Naqash Saqib <naqash_9028@hotmail.com>
2023-08-16 20:41:58 +0500

    Following changes were made

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+//adding toaster
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
+  //function to call toast
+  //const notify = () => toast("Wow so easy!");
+  useEffect(() => {
+    // Show toast message on page load
+    toast.info('Welcome Irzum!', {
+      position: toast.POSITION.TOP_RIGHT,
+      autoClose: 1000, // Auto-close the toast after 5 seconds
+    });
+  }, []);
   //setting variables and their function to set value using useState
   //OnChange or default Value is required to use value of any input
-  const [input1, setInput1] = useState('1');
-  const [input2, setInput2] = useState('2');
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
   const [operator, setOperator] = useState('');
-  const [result, setResult] = useState('Please start using...');
+  const [result, setResult] = useState('null');
 
   //function to set operator
   const handleOperatorClick = (chosenOperator) => {
@@ -19,12 +32,15 @@ function App() {
     setInput1('');
     setInput2('');
     setOperator('');
-    setResult('');
+    setResult('null');
   };
 
   //to change value of input, this on change function is must. And using this function, we change value of the input on change
   const handleNumberChange = (event, inputNumber) => {
-    const value = event.target.value;
+    const min = 1;
+    const max = 100;
+    //const value = event.target.value;
+    const value = Math.max(min, Math.min(max, Number(event.target.value)));
     if (inputNumber === 1) {
       setInput1(value);
     } else if (inputNumber === 2) {
@@ -58,15 +74,35 @@ function App() {
           calculatedResult = '';
       }
     }
-
     setResult(calculatedResult);
+  };
+
+  // Validation function to check if input is non-negative
+  const isNonNegative = (value) => {
+    return parseFloat(value) >= 0 || value === '';
   };
 
   return (
     <div className="calculator">
-      <input type="number" id="input1" name="input1" placeholder="Enter value 1" value={input1} onChange={(e) => handleNumberChange(e, 1)} />
+      <input
+        type="number"
+        id="input1"
+        name="input1"
+        placeholder="Enter value 1"
+        value={input1}
+        onChange={(e) => handleNumberChange(e, 1)}
+        className={!isNonNegative(input1) ? 'error' : ''}
+      />
       <span className="operator">{operator}</span>
-      <input type="number" id="input2" name="input2" placeholder="Enter value 2" value={input2} onChange={(e) => handleNumberChange(e, 2)} />
+      <input
+        type="number"
+        id="input2"
+        name="input2"
+        placeholder="Enter value 2"
+        value={input2}
+        onChange={(e) => handleNumberChange(e, 2)}
+        className={!isNonNegative(input2) ? 'error' : ''}
+      />
 
       <br />
 
@@ -76,10 +112,21 @@ function App() {
       <button id="divide" onClick={() => handleOperatorClick('/')}>/</button>
 
       <br />
+      {/* <button onClick={notify}>Notify!</button> */}
+      <ToastContainer />
+      {/* Conditional show */}
+      {input1 !== 'null' && input1 !== '' && input2 !== 'null' && input2 !== '' && (
+        <>
+          <button id="equal" onClick={handleEqualClick}>=</button>
+          <button id="clear" onClick={handleClearClick}>CLEAR</button>
+        </>
+      )}
 
-      <button id="equal" onClick={handleEqualClick}>=</button>
-      <button id="clear" onClick={handleClearClick}>CLEAR</button>
-      <div className="result">You answer is <br />{result}</div>
+      {/* Condition */}
+      {result !== 'null' && result !== '' && (
+        <div className="result">Your answer is <br />{result}</div>
+      )}
+
     </div>
   );
 }
